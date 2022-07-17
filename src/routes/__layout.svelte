c5d5cdcd9b0d6da228d676d89f95d8f1dc26f687
AkezhanR <rakishev-as@ext.sber-zvuk.com>
2022-07-17 15:36:25 +0600

    feat: request profile data on global file

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,30 @@
 
     import LeftNavBar from "../components/LeftNavBar.svelte";
     import TopNavBar from "../components/TopNavBar.svelte";
+
+    import axios from 'axios';
+    import { userStore } from "../stores/userStore";
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        const profileResponse = await axios.get('http://localhost:8000/api/profile', {
+            headers: {
+                Authorization: localStorage.getItem('accessToken')
+            }
+        });
+        const { solved_tasks, team } = profileResponse.data;
+
+        userStore.update((prevStore) => ({
+            ...prevStore,
+            isLoggedIn: true,
+            username: team.username,
+            teamName: team.name,
+            members: team.members,
+            id: team.id,
+            score: team.score,
+            solvedTasks: solved_tasks
+        }));
+    });
 </script>
 
 <TopNavBar />

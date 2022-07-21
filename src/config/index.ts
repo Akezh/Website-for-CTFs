a46755454769dfa3d2cde70aa9d098aa13b06116
Temirzhan Yussupov <timka2609@gmail.com>
2022-07-21 20:49:07 +0800

    fix: update ports

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
 export const API_URL = import.meta.env.DEV ? "http://localhost:8000" : "https://ctf.ironsoul.lol";
-export const LINK_URL = import.meta.env.DEV ? "http://localhost:8001" : "http://134.122.91.61:8001";
+export const LINK_URL = import.meta.env.DEV ? "http://localhost:8080" : "http://134.122.91.61:8080";
